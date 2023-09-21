f405bbd488c03b4e3e1019b0f08b5780748c2716
faghy <christian.serafino@dueclic.com>
2023-09-21 17:51:46 +0200

    sub-menu changes

--- fag-theme/assets/js/script.js
+++ fag-theme/assets/js/script.js
@@ -6,4 +6,23 @@ document.addEventListener('DOMContentLoaded', function () {
         this.classList.toggle('active'); // Add/Remove the "active" class to the burger menu
         mobileMenu.classList.toggle('active'); // Add/Remove the "active" class to the mobile menu
     });
+
+    const menuItemHasChildren = document.querySelectorAll('.menu-item-has-children');
+
+    menuItemHasChildren.forEach(function (menuItem) {
+        menuItem.addEventListener('mouseenter', function () {
+            const subMenu = this.querySelector('.sub-menu');
+            if (subMenu) {
+                subMenu.style.display = 'block';
+            }
+        });
+
+        menuItem.addEventListener('mouseleave', function () {
+            const subMenu = this.querySelector('.sub-menu');
+            if (subMenu) {
+                subMenu.style.display = 'none';
+            }
+        });
+    });
 });
+
