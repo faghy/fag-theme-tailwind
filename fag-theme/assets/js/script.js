b9f137da7e08f64243b542dd4124d6bae0a44923
faghy <christian.serafino@dueclic.com>
2023-09-22 13:42:34 +0200

    style.css tailwind conversion add screenshot.png

--- fag-theme/assets/js/script.js
+++ fag-theme/assets/js/script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // Menu MOBILE
+    // Mobile Menu
     const burgerMenu = document.querySelector('.burger-menu');
     const mobileMenu = document.querySelector('.mobile-menu');
 
@@ -8,10 +8,31 @@ document.addEventListener('DOMContentLoaded', function () {
         mobileMenu.classList.toggle('active'); // Add/Remove the "active" class to the mobile menu
     });
 
-    // SUB-MENU
+    // Submenu
     const menuItemHasChildren = document.querySelectorAll('.menu-item-has-children');
 
     menuItemHasChildren.forEach(function (menuItem) {
+        const subMenu = menuItem.querySelector('.sub-menu');
+
+        if (subMenu) {
+            // Create an arrow element
+            const arrow = document.createElement('span');
+            arrow.classList.add('arrow-down'); // You can define CSS classes for the arrow
+            arrow.textContent = ' ▼'; // Text for the arrow (customize as needed)
+
+            // Add the arrow to the menu item
+            menuItem.querySelector('a').appendChild(arrow);
+
+            // Handle submenu opening and closing when clicking the arrow
+            arrow.addEventListener('click', function (event) {
+                event.preventDefault(); // Prevent the default link behavior (if present)
+                subMenu.style.display = subMenu.style.display === 'block' ? 'none' : 'block';
+            });
+
+            // Hide the submenu initially
+            subMenu.style.display = 'none';
+        }
+
         menuItem.addEventListener('mouseenter', function () {
             const subMenu = this.querySelector('.sub-menu');
             if (subMenu) {
@@ -27,4 +48,3 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     });
 });
-
